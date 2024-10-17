377de03ef3792d10ba8f8b04ba01dc69e4177fb0
Michal Kluszewski <michal@kluszew.ski>
2024-10-17 16:04:42 -0700

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Michal Kluszewski on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
